c8524c265889c8e77bfcffca52551e48472be8c2
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2022-12-05 12:57:09 +0100

    update game.py

--- game.py
+++ game.py
@@ -1,16 +1,26 @@
 import random
 import json
 
-ROLES = ["mumus", "tunder", "alommano"]
-
-
 class Player:
-    def __init__(self, name) -> None:
+    def __init__(self, name:str) -> None:
         self.name = name
         self.role = None
         self.points = 0
+    
+    def setRole(self, role:str) -> None:
+        """Beállít a játékosnak egy szerepet."""
+        self.role = role
 
-def gameInit():
+    def addPoint(self, point:int) -> None:
+        """Hozzáaddja a pontokat a játékos pontjaihoz."""
+        self.points += point
+
+def playersInit() -> list:
+    """Felveszi a játékosokat névvel.
+    
+    Returns:
+        list: A játékosok listája.
+    """
     playerList = []
     f = open('roles.json')
     data = json.load(f)
@@ -30,26 +40,72 @@ def gameInit():
         name = input()
         playerList.append(Player(name))
 
+    return playerList
+
+def randomRoles(playerList:list) -> list:
+    """Szerepet oszt minden játékoshoz.
+    
+    Returns:
+        list: A játékosok listája szerepekkel.
+    """
+    f = open('roles.json')
+    data = json.load(f)
+    f.close()
+
     withoutRole = playerList[:]
 
     dreamer = random.choice(playerList)
     for player in playerList:
         if player.name == dreamer.name:
-            player.role = "almodo"
+            player.setRole("almodo")
             withoutRole.remove(dreamer)
 
-    for role in data[str(playerCount)].keys():
-        #print(role)
-        for _ in range(int(data[str(playerCount)][role])):
+    for role in data[str(len(playerList))].keys():
+        for _ in range(int(data[str(len(playerList))][role])):
             randomPlayer = random.choice(withoutRole)
-            randomPlayer.role = role
+            randomPlayer.setRole(role)
             withoutRole.remove(randomPlayer)
-        #print(data[str(playerCount)][role])
 
     for player in playerList:
-        print(player.name)
-        print(player.role)
+        print("Név: " + player.name)
+        print("Szerep: " + player.role)
         print()
 
+    return playerList
+
+
+########
+# Terv #
+########
+def round():
+    kartyak = []
+    helyesTippek = []
+    rosszTippek = []
+    almodoTippek = []
+    while True:
+        kartya = None
+        print("Álmodó tipp: ")
+        tipp = input()
+        almodoTippek.append(tipp)
+        if tipp == "exit": break
+        elif helyesTipp: 
+            almodoTippek.append(tipp)
+            helyesTippek.append(tipp)
+        else: 
+            almodoTippek.append(None)
+
+def helyesTipp(kartyaSzo, tipp): False # Balázs írja
+
 if __name__ == "__main__": 
-    gameInit()
+    playerList = playersInit()
+    playerList = randomRoles(playerList)
+    
+    # Terv
+    #option = 0
+    #while option == 0:
+    #    print('[1] új játék')
+    #    print('[2] START')
+    #    if option == 1:
+    #        playerList = playersInit()
+    #    elif option == 2:
+    #        while True: round()
