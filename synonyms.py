2fe1e0891094b586469d0b802ae6f2d3e2330d12
Kebelei Csaba <71121076+kebeleicsaba@users.noreply.github.com>
2022-12-06 00:25:14 +0100

    fix synonyms.py

--- synonyms.py
+++ synonyms.py
@@ -1,17 +1,38 @@
 import requests
-import xmltodict # pip3 install xmltodict
+import xmltodict  # pip3 install xmltodict
 
-def getSynonyms(szo:str) -> list:
+def getSynonyms(szo: str) -> list:
     """Visszaadja egy szó szinonimait.
-    
+    >>> getSynonyms("zöldség")
+    ['saláta', 'sötétség', 'ostobaság', 'hülyeség', 'butaság', 'marhaság', 'badarság', 'szamárság', 'csacsiság', 'bolondság', 'bárgyúság', 'korlátoltság', 'ökörség', 'együgyűség', 'stupiditás', 'blődség', 'sületlenség', 'rossz ötlet']
+    >>> getSynonyms("cica")
+    ['macska', 'kandúr', 'cirmos', 'cicus', 'cicmarek', 'cicamica', 'macskusz', 'cicuska', 'cicuka', 'macsek']
+    >>> getSynonyms("slsdx")
+    ['slsdx']
+    >>> getSynonyms("ciica")
+    ['ciica']
+    >>> getSynonyms("apartman")
+    ['fogadó', 'szálloda', 'hotel', 'szálló', 'motel', 'vendégfogadó', 'panzió', 'vendégház']
+
     Returns:
         list: Szinonimak listája.
     """
     link = f"https://api.poet.hu/szinonima.php?f=leon2001&j=a059763ea5256569f944d98b6d22c947&s={szo}"
     response = requests.get(link)
     content = xmltodict.parse(response.content)
-    if 'hiba' in list(content.keys()):
+    if "hiba" in list(content.keys()):
         return [szo]
-    return content["szinonimak"]["szocsoport"]["szinonima"]
+    elif len(content["szinonimak"]["szocsoport"]) == 1:
+        return content["szinonimak"]["szocsoport"]["szinonima"]
 
-#print(getSynonyms("cica"))
+    synonyms = []
+    for con in content["szinonimak"]["szocsoport"]:
+        if isinstance(con["szinonima"], str):
+            synonyms.append(con["szinonima"])
+        else:
+            synonyms.extend(con["szinonima"])
+    return synonyms
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
