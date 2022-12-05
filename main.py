26c69cd4219a2e0459f1c6bee3121e26b6b6290e
Simon Márton <smarton0614@gmail.com>
2022-12-05 11:09:25 +0100

    added szavak

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-import tkinter as tk
+from tkinter import *
 
-window = tk.Tk()
-greeting = tk.Label(text="Hello, Tkinter")
+
+
+
+szavak = []
+file1 = open('szavak.txt', 'r',encoding='utf-8')
+Lines = file1.readlines()
+for line in Lines:
+    szavak.append(line.strip())
+print(szavak)
+
+
+def createcard():
+    pass
+
+
+
+window = Tk()
+greeting = Label(text="Hello, Tkinter")
 greeting.pack()
 window.geometry("500x500")
+#bg = PhotoImage(file = "wood.png")
+canvas1 = Canvas( window, width = 500,
+                 height = 500)
+canvas1.pack(fill = "both", expand = True)
+#canvas1.create_image( 0, 0, image = bg, 
+#                    anchor = "nw")
+
 window.mainloop()
