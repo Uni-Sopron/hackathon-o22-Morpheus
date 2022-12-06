811054060f93c430ef66ccc2b0fc745367a7e102
Péter Kende <90194875+kendep1@users.noreply.github.com>
2022-12-06 12:26:34 +0100

    Add files via upload

---
+++ szavak_parositasa_en_hu.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+
+angol = []
+file1 = open(my_path+'/words.txt', 'r', encoding='utf-8')
+Lines = file1.readlines()
+for line in Lines:
+    angol.append(line.strip())
+file1.close()
+
+magyar = []
+file2 = open(my_path+'/szavak.txt', 'r', encoding='utf-8')
+Lines = file2.readlines()
+for line in Lines:
+    magyar.append(line.strip())
+file2.close()
+
+en_hu = []
+
+
+for i in range(len(angol)):
+    for j in range(len(magyar)):
+        if i == j:
+            en_hu.append({angol[i]: magyar[j]})
+
+print(en_hu)
+print(en_hu[0]["horse"])
+
+with open("szoparositas.json","w",encoding="UTF8") as f:
+    json.dump(en_hu ,f, ensure_ascii=False)
