3fc66cabae2c535350f69ae03728a9764c2d73e0
babodibalazs <79583287+babodibalazs@users.noreply.github.com>
2022-12-06 11:42:05 +0100

    Optimalizáció

--- pinterest.py
+++ pinterest.py
@@ -1,33 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import random
-import shutil
-from urlextract import URLExtract
 import os
+from game import get_5_szo
 # kell pip installálni: bs4, requests, shutil, urlextract, lxml
 
-
-def readszavak():
-    szavak = []
-    file1 = open('words.txt', 'r', encoding='utf-8')
-    Lines = file1.readlines()
-    for line in Lines:
-        szavak.append(line.strip())
-
-    return szavak
-
-
-def get_5_szo():
-    selected_choices = []
-    szavak = readszavak()
-    for _ in range(5):
-        choice = random.choice(szavak)
-        szavak.remove(choice)
-        selected_choices.append(choice)
-
-    print(selected_choices)
-    return selected_choices
-
+my_path = os.path.abspath(os.path.dirname(__file__))
 
 def give_url(words: list) -> list:
     images = []
@@ -37,13 +14,10 @@ def give_url(words: list) -> list:
     soup = BeautifulSoup(r.text, 'lxml')
     with open("asd.txt", "w",encoding="utf8") as f:
         f.write(f"{soup}")
-            
-            
         #first_image_link = soup.find('a', class_='image-list-link')['href']
         #images.append(first_image_link)
         #print(first_image_link)
     return images
-
 
 def main():
 
@@ -53,6 +27,5 @@ def main():
     urls = give_url(szavak)
 
     print(urls)
-
 
 main()
