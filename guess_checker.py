43e33b21842f292b348fc1b005fb2cbc9bb9d0bf
babodibalazs <79583287+babodibalazs@users.noreply.github.com>
2022-12-06 11:21:02 +0100

    Jó válasz ékezettelenítés

--- guess_checker.py
+++ guess_checker.py
@@ -11,16 +11,20 @@ def is_correct_guess(word: str, testword: str) -> bool:
         return True
     else:
         letters = word
+        test_letters = testword
         for letter in letters_to_check:
             if letters.count(letter,0,len(letters)) != 0:
                 index = letters_to_check.index(letter)
                 letters = letters.replace(letters_to_check[index], letters_to_change_to[index])
-        if letters == testword:
+            if test_letters.count(letter,0,len(test_letters)) != 0:
+                index = letters_to_check.index(letter)
+                test_letters = test_letters.replace(letters_to_check[index], letters_to_change_to[index])
+        if letters == testword or letters == test_letters:
             return True
         else:
             translated_word = Translator(source_language='hu', str_to_translate=word).translate_word()
             translated_testword = Translator(source_language='hu', str_to_translate=testword).translate_word()
-            synonyms = Synonyms(translated_word).find_synonyms()
+            synonyms = Synonyms(translated_word, max_number_of_requests=24).find_synonyms()
             for synonym in synonyms:
                 if synonym == translated_testword:
                     return True
